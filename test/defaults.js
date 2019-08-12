8426b65c641f3cad0276b60bbee94b7d632866ac
Michael Wittig <michael@widdix.de>
2019-08-12 17:24:49 +0200

    improve tests

--- test/defaults.js
+++ test/defaults.js
@@ -11,14 +11,3 @@ test.serial('defaults', async t => {
     t.pass();
   }
 });
-
-test.serial('with-kms', async t => {
-  const stackName = cfntest.stackName();
-  try {
-    t.log(await cfntest.createStack(`${__dirname}/with-kms.yml`, stackName, {}));
-    // what could we test here?
-  } finally {
-    t.log(await cfntest.deleteStack(stackName));
-    t.pass();
-  }
-});
